b31c94020429e80baee8fb27f8170aff366ce006
Rupesh Singh <rupesh@joveo.com>
2020-12-04 10:54:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ def lambda_handler( event, _):
     """Initial entry point for lambda"""
 
     logger = logging.getLogger()
-    data = {"API_URL": os.getenv("SIGNALFX_API"), "TOKEN": os.getenv("SIGNALFX_TOKEN")}
-    integrationsfx = IntegrationSignalfx(data)
-    messagepub = MessagePublisher("SignalFx")
-    users = integrationsfx.get_users()
-    ldap = getconnection()
     dryrun = event.get('dryrun', False)
+    sources = [IntegrationSignalfx()]
+    
+    for source in sources:
+        messagepub = MessagePublisher(source.name())
+        users = source.get_users()
+        ldap = getconnection()
 
-    for user in users:
-        if not isuser(user.get("email"), ldap):
-            if not dryrun:
-                integrationsfx.remove_user(user.get('id'))
-                logger.info("Invalid user found...\nEmail: "+user.get('email')+"\nSignalFx ID: "+user.get('id'))
-                messagepub.publish_message(user.get('email'), user.get('id'))
+        for user in users:
+            if not isuser(user.get("email"), ldap):
+                if not dryrun:
+                    source.remove_user(user.get('id'))
+                    logger.info("Invalid user found...\nEmail: "+user.get('email')+"\nSignalFx ID: "+user.get('id'))
+                    messagepub.publish_message(user.get('email'), user.get('id'))
+                else:
+                    logger.info("dryrun enabled")
             else:
-                logger.info("dryrun enabled")
-        else:
-            logger.info("User validated successfully!...\nEmail: "+user.get('email')+"\nSignalFx ID: "+user.get('id'))
+                logger.info("User validated successfully!...\nEmail: "+user.get('email')+"\nSignalFx ID: "+user.get('id'))
 
 if __name__ == "__main__":
     EVENT = {'dryrun': True}
