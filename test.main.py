4488aec6b3b710f0b0da0d333f1e781c33f7746a
Rupesh Singh <rupesh@joveo.com>
2020-12-07 18:52:04 +0530

    Update test.main.py

--- test.main.py
+++ test.main.py
@@ -17,15 +17,38 @@ class TestLambdaHandler(unittest.TestCase):
       main.sources = []
       isUser = MagicMock()
       ldap = MagicMock()
-      sources = MagicMock()
+      source = MagicMock()
       msg_publisher = MagicMock()
       mock_is_user.return_value = isUser
       mock_ldap.return_value = ldap
-      mock_source.return_value = sources
+      mock_source.return_value = source
       mock_msg_publisher.return_value = msg_publisher
       event = {'dryrun': False}
       lambda_handler(event, None)
       mock_is_user.assert_not_called()
-      mock_ldap.assert_not_called()
+      mock_ldap.assert_called_once()
       mock_source.assert_not_called()
       mock_msg_publisher.assert_not_called()
+    
+    @mock.patch('main.MessagePublisher')
+    @mock.patch('main.IntegrationSignalfx')
+    @mock.patch('main.getconnection')
+    @mock.patch('main.isUser')
+    def test_empty_user_from_sources(self, mock_is_user, mock_ldap, mock_source, mock_msg_publisher):
+      isUser = MagicMock()
+      ldap = MagicMock()
+      source = MagicMock()
+      msg_publisher = MagicMock()
+      main.sources = [source]
+      mock_is_user.return_value = isUser
+      mock_ldap.return_value = ldap
+      mock_source.return_value = source
+      mock_msg_publisher.return_value = msg_publisher
+      source.get_users.return_value = []
+      event = {'dryrun': False}
+      lambda_handler(event, None)
+      mock_is_user.assert_not_called()
+      mock_ldap.assert_called_once()
+      mock_source.assert_not_called()
+      mock_msg_publisher.assert_not_called()
+      source.get_users.assert_called_once()
