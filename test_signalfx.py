343bdb2778e5f0784dcbd90e6af5b05f53cc6763
Rupesh Singh <rupesh@joveo.com>
2020-12-10 14:41:20 +0530

    Update test_signalfx.py

--- test_signalfx.py
+++ test_signalfx.py
@@ -15,15 +15,25 @@ class TestIntegrationSignalfx(unittest.TestCase):
     def setUp(self):
       self.signalfxsource = IntegrationSignalfx({"API_URL": "https://signalfx.com", "TOKEN": "abcdef"})
     
+    @mock.patch('sendevents.requests.get', side_effect={'count':0})
+    def test_get_count_zero(self):
+      count_result = self.signalfxsource.get_count('https://signalfx.com', [])
+      self.assertEqual(0, count_result)  
+      
+    @mock.patch('sendevents.requests.get', side_effect={'count':1})
+    def test_get_count_postive_value(self):
+      count_result = self.signalfxsource.get_count('https://signalfx.com', [])
+      self.assertEqual(1, count_result)   
+      
     @mock.patch('sendevents.requests.delete', side_effect=MockResponse(204))
     def test_remove_user_success(self):
       delete_result = self.signalfxsource.remove_user('abc')
-      self.assertEqual(204, delete_result)
+      self.assertEqual(True, delete_result)
     
     @mock.patch('sendevents.requests.delete', side_effect=MockResponse(500))
     def test_remove_user_fail(self):
       delete_result = self.signalfxsource.remove_user('abc')
-      self.assertEqual(500, delete_result) 
+      self.assertEqual(False, delete_result) 
       
     def test_integration_name(self):
       integration_name = self.signalfxsource.name()
